5785cc2cfba816ef1bb4879f6834fa6e2efa6b84
shivpal <Shivpal11singh@gmail.com>
2018-06-22 12:30:11 +0530

    SQL
    Signed-off-by: shivpal <Shivpal11singh@gmail.com>

---
+++ DBManager.php
@@ -0,0 +1,63 @@
+<?php 
+
+class DBManager {
+
+	public $client;
+	
+	/*
+	* initializes mysql with the given credentials
+	*/
+	
+	public function __construct() {
+		$lines = file("Config.properties"); 
+		foreach ($lines as $line) {
+			list($k, $v) = explode('=', $line);
+			if (rtrim(ltrim($k)) == rtrim(ltrim("app42.paas.db.username"))) {
+				$user = rtrim(ltrim($v));
+			}if (rtrim(ltrim($k)) == rtrim(ltrim("app42.paas.db.port"))) {
+				$port = rtrim(ltrim($v));
+			}if (rtrim(ltrim($k)) == rtrim(ltrim("app42.paas.db.password"))) {
+				$password = rtrim(ltrim($v));
+			}if (rtrim(ltrim($k)) == rtrim(ltrim("app42.paas.db.ip"))) {
+				$ip = rtrim(ltrim($v));
+			}if (rtrim(ltrim($k)) == rtrim(ltrim("app42.paas.db.name"))) {
+				$dbName = rtrim(ltrim($v));
+			}
+		}
+			$this->client = mysqli_connect("$ip:$port", $user, $password, $dbName)
+			or die("Unable to connect to MySQL");
+			$this->selected = mysqli_select_db($this->client,$dbName) or die(mysqli_connect_error());
+	  
+    }
+
+
+	/*
+	* insert data into mysql (user table)
+	*/
+	
+	function insertData($name, $email, $description) {
+		
+		try{
+			$sql = "CREATE TABLE user(name VARCHAR(255), email VARCHAR(355), description TEXT)";
+			mysqli_query($this->client,$sql);
+		}catch(Exception $e){
+				print_r("Table Already Created");
+		}
+		
+		mysqli_query($this->client,"INSERT INTO user(name,email,description) VALUES('$name','$email','$description')");
+    }
+	
+	/*
+	* get all data from mysql (user table)
+	*/
+	
+	function getAllData() {
+		
+		$result = mysqli_query($this->client,"select * from user");
+		return $result;
+		
+    }
+
+}
+
+?>
